96d5199228f737f2b2340e719e234e4280c0d7ff
Subramaniam Balasegaran <blsgrn@protonmail.com>
2023-01-03 09:09:58 +0530

    add navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100] bg-slate-300">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-10">
+        <Image
+          src="/../public/assets/unsplash01.jpg"
+          alt="/"
+          height="125"
+          width="50"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Tech News
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
