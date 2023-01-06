8ec88f8fec08636499d1ef16352bc69b6ed8e128
Subramaniam Balasegaran <blsgrn@protonmail.com>
2023-01-06 05:31:27 +0530

    Add image to side bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import { AiOutlineMenu } from "react-icons/ai";
 function Navbar() {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100] bg-slate-300">
@@ -28,6 +28,23 @@ function Navbar() {
               </li>
             </Link>
           </ul>
+          <div className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+        <div className="fixed left-0 top-0 w-[75%] h-screen sm:w-[60%] md:w-[45%] bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div>
+            <div>
+              <Image
+                src="/../public/assets/unsplash01.jpg"
+                alt="/"
+                height="125"
+                width="50"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
